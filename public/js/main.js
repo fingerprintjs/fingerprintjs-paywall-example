16f22fa3172a4eaf3014b5fc40a7faa4ce082c51
kalexey <a.kuznetsov@draewil.com>
2020-07-13 19:40:51 +0300

    refactor: Remove unused code related to contact form

--- public/js/main.js
+++ public/js/main.js
@@ -15,36 +15,3 @@ var fpLoaded = function (fp) {
     });
   });
 }
-$("#subscribe-btn").on("click", function () {
-  $("#paywall-1").addClass("hide");
-  $("#paywall-2").removeClass("hide");
-  setTimeout(function(){
-    $("#contact-email").focus();
-  },7000);
-});
-
-$("#contact-form").on("submit", function (e) {
-  var $submit = $("#contact-submit");
-  $submit.prop("disabled", true);
-  e.preventDefault();
-  var email = $("#contact-email").val();
-  if (!email) {
-    return alert("Please enter your email");
-  }
-  var payload = { email: email, name: email, website: email, comment: 'paywall.fpjs.dev' };
-  $.ajax({
-    url: "https://admin.fingerprintjs.com/leads",
-    type: 'post',
-    dataType: 'json',
-    contentType: 'application/json',
-    data: JSON.stringify(payload),
-    success: function () {
-      $('#paywall-modal').modal('hide');
-      setTimeout(() => { alert('Thanks, we received your data') }, 300);
-    },
-  }).catch(function () {
-    alert("Error occurred, contact us at: support@fingerprintjs.com");
-  }).always(function () {
-    $submit.prop("disabled", false);
-  });
-});
