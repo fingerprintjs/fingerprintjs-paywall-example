3aa30c6f3e5cf6129b36bb2790e8b21637539b7a
Sergey M <finesserus@gmail.com>
2021-06-15 16:56:47 +1000

    Use the experimental version of @fingerprintjs/fingerprintjs-pro

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,24 @@
-var fpLoaded = function (fp) {
+function fpAgentLoaded() {
   var match = location.href.match(/articles\/(\d+)$/)
   if(!match){
     return
   }
-  fp.get().then(function (res) {
-    var query = {
-      visitorId: res.visitorId,
-      articleId: match[1]
-    };
-    $.getJSON("/paywall", query, function (res) {
-      if(res.enabled){
-        $("#paywall-modal").modal({ keyboard: false, show: true, backdrop: "static" });
-      }
-    });
-  });
+  FingerprintJS.load({
+    token: 'paywall',
+    endpoint: 'https://f.fingerprintjs.com/',
+  })
+    .then(function (fp) {
+      return fp.get()
+    })
+    .then(function (res) {
+      var query = {
+        visitorId: res.visitorId,
+        articleId: match[1]
+      };
+      $.getJSON("/paywall", query, function (res) {
+        if(res.enabled){
+          $("#paywall-modal").modal({ keyboard: false, show: true, backdrop: "static" });
+        }
+      });
+    })
 }
